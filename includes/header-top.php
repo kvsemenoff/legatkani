4ee96ddb5d97c10bc890bc9e7b7f7e207bddf8a8
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-05-11 11:18:12 +0300

    header-top

---
+++ includes/header-top.php
@@ -0,0 +1,35 @@
+<div class="header-top">
+  <div class="container">
+      <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="search-wrap hidden-sm hidden-md hidden-lg"> <!-- Мобайл -->
+    			<input type="text" placeholder="Поиск...">
+    			<input type="submit">
+    		</div>
+        <div class="search-wrap hidden-xs"> <!-- Десктоп -->
+    			<input type="text" placeholder="Поиск по сайту...">
+    			<input type="submit">
+    		</div>
+      </div>
+      <div class="col-xs-6 col-sm-6 col-md-6">
+        <div class="header_auth">
+          <ul class="header_auth_list">
+            <li class="hidden-xs">
+              <a href="#" class="header_auth_list_link signin">Войти</a>
+            </li>
+            <li class="hidden-xs">
+              <a href="#" class="header_auth_list_link signin">Регистрация</a>
+            </li>
+            <li class="hidden-sm hidden-md hidden-lg">
+              <a href="#" class="header_auth_list_link auth"></a>
+            </li>
+            <li>
+              <a href="#" class="header_auth_list_link favourite">0</a>
+            </li>
+            <li>
+              <a href="#" class="header_auth_list_link basket">2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+  </div>
+</div>
