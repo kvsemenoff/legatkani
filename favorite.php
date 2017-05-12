d8a734c02bfe5a36a67616cbff11003bb7ad6a20
engine77 <zaur.1kager@gmail.com>
2017-05-12 17:41:27 +0400

    all file

--- favorite.php
+++ favorite.php
@@ -7,6 +7,157 @@
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">
 
+
+			<div class="container paddingnone">
+				<h2 class="h2 favorite__caption">Избранное</h2>
+				<ul class="breadcrumps ">
+					<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Главная</a></li>				
+					<li class="breadcrumps__link"><span>Избранное</span></li>
+				</ul>
+				
+				<div class="favorite">
+					<h4 class="favorite__alert">Внимание!</h4>
+					<span class="favorite__subtxt">Товары добавленные в избранное хранятся 7 дней</span>					
+					
+						<div class="favorite__block">	
+							<div class="tovar_item favorite__tovar_item">
+								<div class="tovar_img ">
+									<div class="tovar_img__img">
+										<a href="#"><img src="img/tkan4.jpg" alt=""></a>
+										<div class="tovar_img__hover">
+											<div class="tovar_img__border">
+												<a href="#">Подробнее ></a>
+											</div>			
+										</div>
+									</div>
+									<a href="#" class="favorite__closetop"></a>									
+								</div>
+								<div class="tovar_item__text">
+									<span class="tovar_item__nameproduct">Бархат</span>
+									<span class="tovar_item__price"><b>520 </b>руб/м</span>
+									<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+								</div>
+							</div>
+							<div class="tovar_item favorite__tovar_item">
+								<div class="tovar_img ">
+									<div class="tovar_img__img">
+										<a href="#"><img src="img/tkan7.jpg" alt=""></a>
+										<div class="tovar_img__hover">
+											<div class="tovar_img__border">
+												<a href="#">Подробнее ></a>
+											</div>			
+										</div>
+									</div>
+									<a href="#" class="favorite__closetop"></a>									
+								</div>
+								<div class="tovar_item__text">
+									<span class="tovar_item__nameproduct">Кружево</span>
+									<span class="tovar_item__price"><b>650 </b>руб/м</span>
+									<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+								</div>
+							</div>	
+							<div class="tovar_item favorite__tovar_item">
+								<div class="tovar_img ">
+									<div class="tovar_img__img">
+										<a href="#"><img src="img/tkan6.jpg" alt=""></a>
+										<div class="tovar_img__hover">
+											<div class="tovar_img__border">
+												<a href="#">Подробнее ></a>
+											</div>			
+										</div>
+									</div>
+									<a href="#" class="favorite__closetop"></a>									
+								</div>
+								<div class="tovar_item__text">
+									<span class="tovar_item__nameproduct">Батист</span>
+									<span class="tovar_item__price"><b>710 </b>руб/м</span>
+									<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+								</div>
+							</div>	
+							<div class="tovar_item favorite__tovar_item">
+								<div class="tovar_img ">
+									<div class="tovar_img__img">
+										<a href="#"><img src="img/tkan4.jpg" alt=""></a>
+										<div class="tovar_img__hover">
+											<div class="tovar_img__border">
+												<a href="#">Подробнее ></a>
+											</div>			
+										</div>
+									</div>
+									<a href="#" class="favorite__closetop"></a>									
+								</div>
+								<div class="tovar_item__text">
+									<span class="tovar_item__nameproduct">Кружево</span>
+									<span class="tovar_item__price"><b>650 </b>руб/м</span>
+									<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+								</div>
+							</div>	
+							<div class="tovar_item favorite__tovar_item">
+								<div class="tovar_img ">
+									<div class="tovar_img__img">
+										<a href="#"><img src="img/tkan4.jpg" alt=""></a>
+										<div class="tovar_img__hover">
+											<div class="tovar_img__border">
+												<a href="#">Подробнее ></a>
+											</div>			
+										</div>
+									</div>
+									<a href="#" class="favorite__closetop"></a>									
+								</div>
+								<div class="tovar_item__text">
+									<span class="tovar_item__nameproduct">Бархат</span>
+									<span class="tovar_item__price"><b>520 </b>руб/м</span>
+									<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+								</div>
+							</div>
+							<div class="tovar_item favorite__tovar_item">
+								<div class="tovar_img ">
+									<div class="tovar_img__img">
+										<a href="#"><img src="img/tkan7.jpg" alt=""></a>
+										<div class="tovar_img__hover">
+											<div class="tovar_img__border">
+												<a href="#">Подробнее ></a>
+											</div>			
+										</div>
+									</div>
+									<a href="#" class="favorite__closetop"></a>									
+								</div>
+								<div class="tovar_item__text">
+									<span class="tovar_item__nameproduct">Кружево</span>
+									<span class="tovar_item__price"><b>650 </b>руб/м</span>
+									<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+								</div>
+							</div>	
+							<div class="tovar_item favorite__tovar_item">
+								<div class="tovar_img ">
+									<div class="tovar_img__img">
+										<a href="#"><img src="img/tkan6.jpg" alt=""></a>
+										<div class="tovar_img__hover">
+											<div class="tovar_img__border">
+												<a href="#">Подробнее ></a>
+											</div>			
+										</div>
+									</div>
+									<a href="#" class="favorite__closetop"></a>									
+								</div>
+								<div class="tovar_item__text">
+									<span class="tovar_item__nameproduct">Батист</span>
+									<span class="tovar_item__price"><b>710 </b>руб/м</span>
+									<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+								</div>
+							</div>				
+
+							
+						</div>
+				
+
+
+
+				</div>
+
+			</div>	
+
+
 		</div>
 		<div class="n-footer">
 			<?php require_once('includes/footer.php'); ?>
