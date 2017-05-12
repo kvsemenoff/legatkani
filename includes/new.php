a5a461886a2a87a50a906c69fe9cd2a668d48132
engine77 <zaur.1kager@gmail.com>
2017-05-12 13:04:20 +0400

    all file

--- includes/new.php
+++ includes/new.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <div class="newcontainer">
 	<div class="new_block">
 
@@ -77,22 +77,22 @@
 
 	</div>
 </div>
-=======
+
 <div class="new">
-  <div class="new_head">
-    <p class="new_head__title">
-      Звоните нам или пишите прямо сейчас!
-    </p>
-  </div>
-  <div class="new_numbers">
-    <p class="new_numbers__phone">+7 495 514-09-61</p>
-    <p class="new_numbers__phone">+7 495 926-47-18</p>
-  </div>
-  <div class="new_feedback">
-    <p class="new_feedback__text">
-      Вы также можете связаться с нами по e-mail: <a href="mailto:info@legatkani.ru">info@legatkani.ru</a>
-или через <a href="#">форму обратной связи</a>
-    </p>
-  </div>
+	<div class="new_head">
+		<p class="new_head__title">
+			Звоните нам или пишите прямо сейчас!
+		</p>
+	</div>
+	<div class="new_numbers">
+		<p class="new_numbers__phone">+7 495 514-09-61</p>
+		<p class="new_numbers__phone">+7 495 926-47-18</p>
+	</div>
+	<div class="new_feedback">
+		<p class="new_feedback__text">
+			Вы также можете связаться с нами по e-mail: <a href="mailto:info@legatkani.ru">info@legatkani.ru</a>
+			или через <a href="#">форму обратной связи</a>
+		</p>
+	</div>
 </div>
->>>>>>> 1eaf1c744733346ab4be585084adda8aea2e8487
+
