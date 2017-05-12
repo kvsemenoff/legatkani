4f5d881f7912707ef89a46b500bdc32fb2399a5d
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-05-12 14:57:32 +0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,68 @@
+    <div class="footer">
+      <div class="container">
+        <div class="col-xs-12 col-sm-6 col-lg-6 footer__left">
+          <div class="footer__logo-block">
+            <div class="logobox">
+            	<div class="logobox__txt logobox__txt_bottom">
+            		<span>Склад итальянских тканей</span>
+            	</div>
+            	<div class="logobox__img logobox__img_bot">
+            		<img src="../../img/logo2.png" alt="">
+            	</div>
+            </div>
+            <p class="hidden-xs footer__logo-block_copy">
+              © «Lega Tessile», 2008–2017г. Все права защищены
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-lg-6 footer__right">
+          <div class="footer__socials hidden-xs">
+            <div class="social social__style">
+            	<ul>
+            		<li><a href="#"><img src="img/vk.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/ok.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/face.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/goog.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/inst.png" alt=""></a></li>
+            	</ul>
+            </div>
+          </div>
+          <div class="footer__map">
+            <div class="footer__map__structure">
+              <ul class="footer__map__structure_list structure_list_left hidden-xs">
+                <li class="footer__map__structure_item"><a class="footer__map__structure_link" href="#">Новости</a></li>
+                <li class="footer__map__structure_item"><a class="footer__map__structure_link" href="#">Другое</a></li>
+              </ul>
+              <ul class="footer__map__structure_list structure_list_right">
+                <li class="footer__map__structure_item"><a class="footer__map__structure_link" href="#">Карта сайта</a></li>
+                <li class="footer__map__structure_item"><a class="footer__map__structure_link" href="#">Новости</a></li>
+                <li class="footer__map__structure_item"><a class="footer__map__structure_link" href="#">Статьи</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="footer__socials hidden-sm hidden-md hidden-lg">
+            <div class="social social__style">
+            	<ul>
+            		<li><a href="#"><img src="img/vk.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/ok.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/face.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/goog.png" alt=""></a></li>
+            		<li><a href="#"><img src="img/inst.png" alt=""></a></li>
+            	</ul>
+            </div>
+          </div>
+          <p class="hidden-sm hidden-md hidden-lg footer__logo-block_copy">
+            © «Lega Tessile», 2008–2017г. Все права защищены
+          </p>
+          <div class="footer__map__made">
+            <p class="footer__map__made_text">Сделано в компании <a href="#">“Ютекс Проект”</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer__line">
 
+    </div>
  		<!-- JS_BLOCK -->
  		<script src="libs/jquery/jquery-1.11.1.min.js"></script>
  		<script src="libs/owl.carousel/owl.carousel.js"></script>
