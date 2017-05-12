90c12e2cd93a5258733a3dce0252f241f799bd6f
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-05-12 11:52:32 +0300

    map

---
+++ includes/new.php
@@ -0,0 +1,17 @@
+<div class="new">
+  <div class="new_head">
+    <p class="new_head__title">
+      Звоните нам или пишите прямо сейчас!
+    </p>
+  </div>
+  <div class="new_numbers">
+    <p class="new_numbers__phone">+7 495 514-09-61</p>
+    <p class="new_numbers__phone">+7 495 926-47-18</p>
+  </div>
+  <div class="new_feedback">
+    <p class="new_feedback__text">
+      Вы также можете связаться с нами по e-mail: <a href="mailto:info@legatkani.ru">info@legatkani.ru</a>
+или через <a href="#">форму обратной связи</a>
+    </p>
+  </div>
+</div>
