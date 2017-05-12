f8f51736417d78e89b6cf7cd491c8306652cea67
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-05-12 18:39:10 +0300

    lace

--- lace.php
+++ lace.php
@@ -8,7 +8,7 @@
 		<div class="n-content-inner vertical-align">
 			<div class="lace">
 				<div class="container lace-container">
-					<div class="col-lg-3">
+					<div class="col-sm-12 col-md-3">
 						<div class="lace__sidebar">
 							<div class="sidebar-menu sidebar-menu__style">
 								<ul class="sidebar-menu__item">
@@ -40,156 +40,192 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-lg-9">
-						<ul class="filter">
-							<li class="filter__menu">
-								<a href="#" class="filter__link">тип ткани</a>
-								<ul class="filter__hover_menu">
-									<li><a href="#">шелк натуральный</a></li>
-									<li><a href="#">хлопок</a></li>
-									<li><a href="#">полиэстр</a></li>
-								</ul>
-							</li>
-							<li class="filter__menu">
-								<a href="#" class="filter__link">цвет</a>
-								<ul class="filter__hover_menu">
-									<li><a href="#">шелк натуральный</a></li>
-									<li><a href="#">хлопок</a></li>
-									<li><a href="#">полиэстр</a></li>
-								</ul>
-							</li>
-							<li class="filter__menu">
-								<a href="#" class="filter__link">вид дизайна</a>
-								<ul class="filter__hover_menu">
-									<li><a href="#">шелк натуральный</a></li>
-									<li><a href="#">хлопок</a></li>
-									<li><a href="#">полиэстр</a></li>
-								</ul>
-							</li>
-							<li class="filter__menu">
-								<a href="#" class="filter__link">состав</a>
-								<ul class="filter__hover_menu">
-									<li><a href="#">шелк натуральный</a></li>
-									<li><a href="#">хлопок</a></li>
-									<li><a href="#">полиэстр</a></li>
-								</ul>
-							</li>
-							<li class="filter__menu">
-								<a href="#" class="filter__link">ширина</a>
-								<ul class="filter__hover_menu">
-									<li><a href="#">шелк натуральный</a></li>
-									<li><a href="#">хлопок</a></li>
-									<li><a href="#">полиэстр</a></li>
-								</ul>
-							</li>
-							<li class="filter__menu">
-								<a href="#" class="filter__link">назанчение</a>
-								<ul class="filter__hover_menu">
-									<li><a href="#">шелк натуральный</a></li>
-									<li><a href="#">хлопок</a></li>
-									<li><a href="#">полиэстр</a></li>
-								</ul>
-							</li>
-							<li class="filter__menu">
-								<a href="#" class="filter__link">цена</a>
-								<ul class="filter__hover_menu">
-									<li><a href="#">шелк натуральный</a></li>
-									<li><a href="#">хлопок</a></li>
-									<li><a href="#">полиэстр</a></li>
-								</ul>
-							</li>
-							<li class="filter__clear">
-								<a href="#">Сбросить</a>
-							</li>
-						</ul>
-						<div class="clear"></div>
-
-						<div class="filter_sort">
-							<div class="filter__vid">
-								<span>вид:</span>
-								<a href="#" class="vid__table"></a>
-								<a href="#" class="vid__string"></a>
-							</div>
-							<div class="filter__sortbox">
-								<span>сортировать:</span>
-								<a href="#" class="sort__price_sort">Цена по возрастанию</a>
-								<a href="#" class="showprice_link sort__showprice">Показать цены в У.Е.</a>
-							</div>
-							<div class="filter__onpage">
-								<span>на странице: </span>
-								<a href="#" class="filter__onpage__checked">20</a>
-								<a href="#">40</a>
-								<a href="#">60</a>
-							</div>
+					<div class="col-sm-12 col-md-9">
+						<div class="lace__header">
+							<h1 class="h1">Кружево</h1>
+						</div>
+						<div class="lace__breadcrumbs">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Главная</a></li>
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Каталог</a></li>
+								<li class="breadcrumps__link"><span>Кружево</span></li>
+							</ul>
+						</div>
+						<div class="lace__filter">
+							<ul class="filter">
+								<li class="filter__menu">
+									<a href="#" class="filter__link">тип ткани</a>
+									<ul class="filter__hover_menu">
+										<li><a href="#">шелк натуральный</a></li>
+										<li><a href="#">хлопок</a></li>
+										<li><a href="#">полиэстр</a></li>
+									</ul>
+								</li>
+								<li class="filter__menu">
+									<a href="#" class="filter__link">цвет</a>
+									<ul class="filter__hover_menu">
+										<li><a href="#">шелк натуральный</a></li>
+										<li><a href="#">хлопок</a></li>
+										<li><a href="#">полиэстр</a></li>
+									</ul>
+								</li>
+								<li class="filter__menu">
+									<a href="#" class="filter__link">вид дизайна</a>
+									<ul class="filter__hover_menu">
+										<li><a href="#">шелк натуральный</a></li>
+										<li><a href="#">хлопок</a></li>
+										<li><a href="#">полиэстр</a></li>
+									</ul>
+								</li>
+								<li class="filter__menu">
+									<a href="#" class="filter__link">состав</a>
+									<ul class="filter__hover_menu">
+										<li><a href="#">шелк натуральный</a></li>
+										<li><a href="#">хлопок</a></li>
+										<li><a href="#">полиэстр</a></li>
+									</ul>
+								</li>
+								<li class="filter__menu">
+									<a href="#" class="filter__link">ширина</a>
+									<ul class="filter__hover_menu">
+										<li><a href="#">шелк натуральный</a></li>
+										<li><a href="#">хлопок</a></li>
+										<li><a href="#">полиэстр</a></li>
+									</ul>
+								</li>
+								<li class="filter__menu">
+									<a href="#" class="filter__link">назанчение</a>
+									<ul class="filter__hover_menu">
+										<li><a href="#">шелк натуральный</a></li>
+										<li><a href="#">хлопок</a></li>
+										<li><a href="#">полиэстр</a></li>
+									</ul>
+								</li>
+								<li class="filter__menu">
+									<a href="#" class="filter__link">цена</a>
+									<ul class="filter__hover_menu">
+										<li><a href="#">шелк натуральный</a></li>
+										<li><a href="#">хлопок</a></li>
+										<li><a href="#">полиэстр</a></li>
+									</ul>
+								</li>
+								<li class="filter__clear">
+									<a href="#">Сбросить</a>
+								</li>
+							</ul>
 							<div class="clear"></div>
-						</div>
 
-						<div class="button__filer">
-							<span>Фильтр</span>
-						</div>
-						<div class="filer__accordion">
-							<div class="filer__accordion__price">
-								<a href="#" class="showprice_link">Показать цены в У.Е.</a>
-								<a href="#" class="sort__price_sort">Цена по возрастанию</a>
+							<div class="filter_sort">
+								<div class="filter__vid">
+									<span>вид:</span>
+									<a href="#" class="vid__table"></a>
+									<a href="#" class="vid__string"></a>
+								</div>
+								<div class="filter__sortbox">
+									<span>сортировать:</span>
+									<a href="#" class="sort__price_sort">Цена по возрастанию</a>
+									<a href="#" class="showprice_link sort__showprice">Показать цены в У.Е.</a>
+								</div>
+								<div class="filter__onpage">
+									<span>на странице: </span>
+									<a href="#" class="filter__onpage__checked">20</a>
+									<a href="#">40</a>
+									<a href="#">60</a>
+								</div>
+								<div class="clear"></div>
 							</div>
 
-							<div class="filer__accordion_box">
-								<a href="js-show-color" class="filer__accordion_link">цвет</a>
-								<div class="filer__accordion_show js-show-color">
-									<a href="#">шелк натуральный</a>
-									<a href="#">хлопок</a>
-									<a href="#">полиэстр</a>
-									<a href="#">эластан</a>
+							<div class="button__filer">
+								<span>Фильтр</span>
+							</div>
+							<div class="filer__accordion">
+								<div class="filer__accordion__price">
+									<a href="#" class="showprice_link">Показать цены в У.Е.</a>
+									<a href="#" class="sort__price_sort">Цена по возрастанию</a>
+								</div>
+
+								<div class="filer__accordion_box">
+									<a href="js-show-color" class="filer__accordion_link">цвет</a>
+									<div class="filer__accordion_show js-show-color">
+										<a href="#">шелк натуральный</a>
+										<a href="#">хлопок</a>
+										<a href="#">полиэстр</a>
+										<a href="#">эластан</a>
+									</div>
+								</div>
+								<div class="filer__accordion_box">
+									<a href="js-show-view" class="filer__accordion_link">вид дизайна</a>
+									<div class="filer__accordion_show js-show-view">
+										<a href="#">шелк натуральный</a>
+										<a href="#">хлопок</a>
+										<a href="#">полиэстр</a>
+										<a href="#">эластан</a>
+									</div>
+								</div>
+								<div class="filer__accordion_box">
+									<a href="js-show-inside" class="filer__accordion_link">состав</a>
+									<div class="filer__accordion_show js-show-inside">
+										<a href="#">шелк натуральный</a>
+										<a href="#">хлопок</a>
+										<a href="#">полиэстр</a>
+										<a href="#">эластан</a>
+									</div>
+								</div>
+								<div class="filer__accordion_box">
+									<a href="js-show-width" class="filer__accordion_link">ширина</a>
+									<div class="filer__accordion_show js-show-width">
+										<a href="#">шелк натуральный</a>
+										<a href="#">хлопок</a>
+										<a href="#">полиэстр</a>
+										<a href="#">эластан</a>
+									</div>
+								</div>
+								<div class="filer__accordion_box">
+									<a href="js-show-for" class="filer__accordion_link">назначение</a>
+									<div class="filer__accordion_show js-show-for">
+										<a href="#">шелк натуральный</a>
+										<a href="#">хлопок</a>
+										<a href="#">полиэстр</a>
+										<a href="#">эластан</a>
+									</div>
+								</div>
+								<div class="filer__accordion_box">
+									<a href="js-show-price" class="filer__accordion_link">цена</a>
+									<div class="filer__accordion_show js-show-price">
+										<a href="#">шелк натуральный</a>
+										<a href="#">хлопок</a>
+										<a href="#">полиэстр</a>
+										<a href="#">эластан</a>
+									</div>
+								</div>
+								<div class="filer__accordion_clear">
+									<a href="#" class="filer__accordion_hide"><span>Свернуть фильтр</span></a>
+									<a href="#" class="filer__accordion_clearlink"><span>Сбросить</span></a>
 								</div>
 							</div>
-							<div class="filer__accordion_box">
-								<a href="js-show-view" class="filer__accordion_link">вид дизайна</a>
-								<div class="filer__accordion_show js-show-view">
-									<a href="#">шелк натуральный</a>
-									<a href="#">хлопок</a>
-									<a href="#">полиэстр</a>
-									<a href="#">эластан</a>
+						</div>
+						<div class="lace__content">
+							<div class="col-md-4">
+								<div class="lace__item">
+									<div class="tovar_item">
+										<div class="tovar_img">
+											<div class="tovar_img__img">
+												<img src="../../img/tkan2.png" alt="">
+												<div class="tovar_img__hover">
+													<div class="tovar_img__border">
+														<a href="#">Подробнее ></a>
+													</div>
+												</div>
+											</div>
+											<div class="tovar_img__toplent tovar_img__toplent_new"><span>новинка</span></div>
+										</div>
+										<div class="tovar_item__text">
+											<span class="tovar_item__nameproduct">Бархат</span>
+											<span class="tovar_item__price"><b>25 </b>у.е./м</span>
+										</div>
+										<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+									</div>
 								</div>
-							</div>
-							<div class="filer__accordion_box">
-								<a href="js-show-inside" class="filer__accordion_link">состав</a>
-								<div class="filer__accordion_show js-show-inside">
-									<a href="#">шелк натуральный</a>
-									<a href="#">хлопок</a>
-									<a href="#">полиэстр</a>
-									<a href="#">эластан</a>
-								</div>
-							</div>
-							<div class="filer__accordion_box">
-								<a href="js-show-width" class="filer__accordion_link">ширина</a>
-								<div class="filer__accordion_show js-show-width">
-									<a href="#">шелк натуральный</a>
-									<a href="#">хлопок</a>
-									<a href="#">полиэстр</a>
-									<a href="#">эластан</a>
-								</div>
-							</div>
-							<div class="filer__accordion_box">
-								<a href="js-show-for" class="filer__accordion_link">назначение</a>
-								<div class="filer__accordion_show js-show-for">
-									<a href="#">шелк натуральный</a>
-									<a href="#">хлопок</a>
-									<a href="#">полиэстр</a>
-									<a href="#">эластан</a>
-								</div>
-							</div>
-							<div class="filer__accordion_box">
-								<a href="js-show-price" class="filer__accordion_link">цена</a>
-								<div class="filer__accordion_show js-show-price">
-									<a href="#">шелк натуральный</a>
-									<a href="#">хлопок</a>
-									<a href="#">полиэстр</a>
-									<a href="#">эластан</a>
-								</div>
-							</div>
-							<div class="filer__accordion_clear">
-								<a href="#" class="filer__accordion_hide"><span>Свернуть фильтр</span></a>
-								<a href="#" class="filer__accordion_clearlink"><span>Сбросить</span></a>
 							</div>
 						</div>
 					</div>
