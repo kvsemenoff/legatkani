75dc4d2641ab1643c649d54c05755b28ec85919c
engine77 <zaur.1kager@gmail.com>
2017-05-12 12:56:14 +0400

    all file

---
+++ includes/new.php
@@ -0,0 +1,78 @@
+<div class="newcontainer">
+	<div class="new_block">
+
+		<div class="tovar_item">
+			<div class="tovar_img">
+				<div class="tovar_img__img">
+					<img src="../../img/tkan.png" alt="">
+					<div class="tovar_img__hover">
+						<div class="tovar_img__border">
+							<a href="#">Подробнее ></a>
+						</div>			
+					</div>
+				</div>
+				<div class="tovar_img__toplent tovar_img__toplent_new"><span>новинка</span></div>
+			</div>
+			<div class="tovar_item__text">
+				<span class="tovar_item__nameproduct">Бархат</span>
+				<span class="tovar_item__price"><b>520 </b>руб/м</span>
+				<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+			</div>
+		</div>
+		<div class="tovar_item">
+			<div class="tovar_img">
+				<div class="tovar_img__img">
+					<img src="../../img/tkan.png" alt="">
+					<div class="tovar_img__hover">
+						<div class="tovar_img__border">
+							<a href="#">Подробнее ></a>
+						</div>			
+					</div>
+				</div>
+				<div class="tovar_img__toplent tovar_img__toplent_new"><span>новинка</span></div>
+			</div>
+			<div class="tovar_item__text">
+				<span class="tovar_item__nameproduct">Бархат</span>
+				<span class="tovar_item__price"><b>520 </b>руб/м</span>
+				<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+			</div>
+		</div>
+		<div class="tovar_item">
+			<div class="tovar_img">
+				<div class="tovar_img__img">
+					<img src="../../img/tkan.png" alt="">
+					<div class="tovar_img__hover">
+						<div class="tovar_img__border">
+							<a href="#">Подробнее ></a>
+						</div>			
+					</div>
+				</div>
+				<div class="tovar_img__toplent tovar_img__toplent_new"><span>новинка</span></div>
+			</div>
+			<div class="tovar_item__text">
+				<span class="tovar_item__nameproduct">Бархат</span>
+				<span class="tovar_item__price"><b>520 </b>руб/м</span>
+				<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+			</div>
+		</div>
+		<div class="tovar_item">
+			<div class="tovar_img">
+				<div class="tovar_img__img">
+					<img src="../../img/tkan.png" alt="">
+					<div class="tovar_img__hover">
+						<div class="tovar_img__border">
+							<a href="#">Подробнее ></a>
+						</div>			
+					</div>
+				</div>
+				<div class="tovar_img__toplent tovar_img__toplent_new"><span>новинка</span></div>
+			</div>
+			<div class="tovar_item__text">
+				<span class="tovar_item__nameproduct">Бархат</span>
+				<span class="tovar_item__price"><b>520 </b>руб/м</span>
+				<a href="#" class="button button__basket"><span class="button__basket_txt">В корзину</span></a>
+			</div>
+		</div>
+
+	</div>
+</div>
