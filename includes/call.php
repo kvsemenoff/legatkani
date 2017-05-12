349dc6f5ee2e356a0cb88665964302fb4f680e07
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-05-12 12:06:36 +0300

    call

--- includes/call.php
+++ includes/call.php
@@ -1,16 +1,16 @@
-<div class="new">
+<div class="call">
   <div class="container">
-    <div class="new_head">
-      <p class="new_head__title">
+    <div class="call_head">
+      <p class="call_head__title">
         Звоните нам или пишите прямо сейчас!
       </p>
     </div>
-    <div class="new_numbers">
-      <p class="new_numbers__phone">+7 495 514-09-61</p>
-      <p class="new_numbers__phone">+7 495 926-47-18</p>
+    <div class="call_numbers">
+      <p class="call_numbers__phone">+7 495 514-09-61</p>
+      <p class="call_numbers__phone">+7 495 926-47-18</p>
     </div>
-    <div class="new_feedback">
-      <p class="new_feedback__text">
+    <div class="call_feedback">
+      <p class="call_feedback__text">
         Вы также можете связаться с нами по e-mail: <a href="mailto:info@legatkani.ru">info@legatkani.ru</a>
     или через <a href="#">форму обратной связи</a>
       </p>
