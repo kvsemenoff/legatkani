5d8837f0293d6b23d7d33af366febe8ceb4e6965
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-13 17:53:40 +0600

    news.php

--- news.php
+++ news.php
@@ -6,7 +6,119 @@
 	</div>
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">
-
+			<div class="inner-de news-page">
+				<div class="container">
+					<div class="row">
+						<div class="h1-wrap">
+							<h1 class="h1">Новости/Статьи</h1>
+						</div>
+						<div class="breadcrumps-wrap">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Главная</a></li>				
+								<li class="breadcrumps__link"><span>О нас</span></li>
+							</ul>
+						</div>
+						<div class="news-row clearfix">
+							<div class="news">
+								<a href="#" class="news__img">
+									<div class="news__img-in">
+										<img src="img/news-1.jpg" alt="">
+										<span>Подробнее ></span>
+									</div>
+								</a>
+								<div class="news__text">
+									<a href="#">
+										<i>25 марта новое поступление тканей из Италии “Lega Tessile” успей сегодня!</i>
+										<span>12.05.2017</span>
+									</a>
+								</div>
+							</div>
+							<div class="news">
+								<a href="#" class="news__img">
+									<div class="news__img-in">
+										<img src="img/news-2.jpg" alt="">
+										<span>Подробнее ></span>
+									</div>
+								</a>
+								<div class="news__text">
+									<a href="#">
+										<i>модые вещи из итальянской ткани знаменитого дизайнера Gooma</i>
+										<span>12.05.2017</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="news-row clearfix">
+							<div class="news">
+								<a href="#" class="news__img">
+									<div class="news__img-in">
+										<img src="img/news-1.jpg" alt="">
+										<span>Подробнее ></span>
+									</div>
+								</a>
+								<div class="news__text">
+									<a href="#">
+										<i>25 марта новое поступление тканей из Италии “Lega Tessile” успей сегодня!</i>
+										<span>12.05.2017</span>
+									</a>
+								</div>
+							</div>
+							<div class="news">
+								<a href="#" class="news__img">
+									<div class="news__img-in">
+										<img src="img/news-2.jpg" alt="">
+										<span>Подробнее ></span>
+									</div>
+								</a>
+								<div class="news__text">
+									<a href="#">
+										<i>модые вещи из итальянской ткани знаменитого дизайнера Gooma</i>
+										<span>12.05.2017</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="news-row clearfix">
+							<div class="news">
+								<a href="#" class="news__img">
+									<div class="news__img-in">
+										<img src="img/news-1.jpg" alt="">
+										<span>Подробнее ></span>
+									</div>
+								</a>
+								<div class="news__text">
+									<a href="#">
+										<i>25 марта новое поступление тканей из Италии “Lega Tessile” успей сегодня!</i>
+										<span>12.05.2017</span>
+									</a>
+								</div>
+							</div>
+							<div class="news">
+								<a href="#" class="news__img">
+									<div class="news__img-in">
+										<img src="img/news-2.jpg" alt="">
+										<span>Подробнее ></span>
+									</div>
+								</a>
+								<div class="news__text">
+									<a href="#">
+										<i>модые вещи из итальянской ткани знаменитого дизайнера Gooma</i>
+										<span>12.05.2017</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="pagination-wrap">
+							<ul class="pagination">
+								<li class="pagination__link"><span>1</span></li>
+								<li class="pagination__link"><a href="/" class="pagination__nonactive">2</a></li>
+								<li class="pagination__link"><a href="/" class="pagination__nonactive">3</a></li>
+								<li class="pagination__link pagination__link_home"><a href="/" class="pagination__nonactive">Следующая</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="n-footer">
 			<?php require_once('includes/footer.php'); ?>
@@ -15,11 +127,3 @@
 </div>
 </body>
 </html>
-
-
-
-
-
-
-
-
