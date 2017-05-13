3013c4563555f490e3a22a67c71552351c760fd2
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-13 08:28:42 +0600

    did about, testimonials, curs

--- curs.php
+++ curs.php
@@ -6,7 +6,154 @@
 	</div>
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">
-
+			<div class="inner-de curs">
+				<div class="container">
+					<div class="row">
+						<div class="h1-wrap">
+							<h1 class="h1">Текущий курс</h1>
+						</div>
+						<div class="breadcrumps-wrap">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Главная</a></li>				
+								<li class="breadcrumps__link"><span>Текущий курс</span></li>
+							</ul>
+						</div>
+						<p class="curs__text">Небольшой текст. Каталог еженедельно пополняется новыми тканями европейских коллекций текущего сезона. Это позволяет поддерживать актуальный ассортимент модных и качественных полотен. За несколько лет работы нам удалось наладить прямые поступления тканей от производителя. Благодаря этому мы устанавливаем умеренный ценник.</p>
+						<div class="curs__info">
+							<span>Текущий курс продажи: <strong>EUR</strong> / <i>RUR</i> <b>62,3</b></span>
+						</div>
+						<div class="curs__table">
+							<span class="curs__table-title">Таблица перевода валют в рубли</span>
+							<div class="table table__1">
+								<div class="table__item table__item_first">
+									<div class="table__item_txt"><span>Цена в <b>У.Е.</b></span></div>
+									<div class="table__item_txt table__item_txt2"><span>Цена в <b>РУБ.</b></span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main1">
+									<div class="table__item_txt table__item_main_txt1"><span>1.5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>96.50</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main2">
+									<div class="table__item_txt table__item_main_txt1"><span>2</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>120</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main1">
+									<div class="table__item_txt table__item_main_txt1"><span>2.5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>125.5</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main2">
+									<div class="table__item_txt table__item_main_txt1"><span>3</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>245</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main1">
+									<div class="table__item_txt table__item_main_txt1"><span>3.5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>340.5</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main2">
+									<div class="table__item_txt table__item_main_txt1"><span>4</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>400</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main1">
+									<div class="table__item_txt table__item_main_txt1"><span>4.5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>425.5</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main2">
+									<div class="table__item_txt table__item_main_txt1"><span>5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>450</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main1">
+									<div class="table__item_txt table__item_main_txt1"><span>5.5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>256.5</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main2">
+									<div class="table__item_txt table__item_main_txt1"><span>6</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>600</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main1">
+									<div class="table__item_txt table__item_main_txt1"><span>6.5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>655.5</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main2">
+									<div class="table__item_txt table__item_main_txt1"><span>7</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>700</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main1">
+									<div class="table__item_txt table__item_main_txt1"><span>7.5</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>745.5</span></div>
+								</div>
+								<div class="table__item table__item_main table__item_main2">
+									<div class="table__item_txt table__item_main_txt1"><span>8</span></div>
+									<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>800</span></div>
+								</div>
+							</div>
+							<div class="table table__2">
+								<div class="table__item table__item_first">
+									<div class="table__item_txt"><span>Цена в <b>У.Е.</b></span></div>
+									<div class="table__item_txt table__item_txt2"><span>Цена в <b>РУБ.</b></span></div>
+								</div>
+								<div class="border__left">
+									<div class="table__item table__item_main table__item_main1">
+										<div class="table__item_txt table__item_main_txt1"><span>1.5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>96.50</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main2">
+										<div class="table__item_txt table__item_main_txt1"><span>2</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>120</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main1">
+										<div class="table__item_txt table__item_main_txt1"><span>2.5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>125.5</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main2">
+										<div class="table__item_txt table__item_main_txt1"><span>3</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>245</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main1">
+										<div class="table__item_txt table__item_main_txt1"><span>3.5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>340.5</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main2">
+										<div class="table__item_txt table__item_main_txt1"><span>4</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>400</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main1">
+										<div class="table__item_txt table__item_main_txt1"><span>4.5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>425.5</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main2">
+										<div class="table__item_txt table__item_main_txt1"><span>5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>450</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main1">
+										<div class="table__item_txt table__item_main_txt1"><span>5.5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>256.5</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main2">
+										<div class="table__item_txt table__item_main_txt1"><span>6</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>600</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main1">
+										<div class="table__item_txt table__item_main_txt1"><span>6.5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>655.5</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main2">
+										<div class="table__item_txt table__item_main_txt1"><span>7</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>700</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main1">
+										<div class="table__item_txt table__item_main_txt1"><span>7.5</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>745.5</span></div>
+									</div>
+									<div class="table__item table__item_main table__item_main2">
+										<div class="table__item_txt table__item_main_txt1"><span>8</span></div>
+										<div class="table__item_txt table__item_txt2 table__item_main_txt2"><span>800</span></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="n-footer">
 			<?php require_once('includes/footer.php'); ?>
@@ -15,11 +162,3 @@
 </div>
 </body>
 </html>
-
-
-
-
-
-
-
-
