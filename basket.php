4a0cdf146348dc014f2e7f3d241ac1e4e2db3212
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-13 18:54:12 +0600

    start basket

---
+++ basket.php
@@ -0,0 +1,32 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+		<?php require_once('includes/header_bottom.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<div class="inner-de basket-page">
+				<div class="container">
+					<div class="row">
+						<div class="h1-wrap">
+							<h1 class="h1">Моя корзина</h1>
+						</div>
+						<div class="breadcrumps-wrap">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Главная</a></li>				
+								<li class="breadcrumps__link"><span>Корзина</span></li>
+							</ul>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="n-footer">
+			<?php require_once('includes/footer.php'); ?>
+		</div>
+	</div>
+</div>
+</body>
+</html>
