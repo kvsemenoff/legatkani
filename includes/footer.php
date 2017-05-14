a13835d606c3897d918ac5ce6adb115fb426f3a0
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-14 19:41:33 +0600

    clean files

--- includes/footer.php
+++ includes/footer.php
@@ -66,8 +66,6 @@
  		<script src="libs/jquery/jquery-1.11.1.min.js"></script>
  		<script src="libs/owl.carousel/owl.carousel.js"></script>
  		<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
- 		<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
- 		<script src="js/responsiveTabs.js"></script>
     <script src="js/jquery.maskedinput.min.js"></script>
  		<script src="js/jquery.formstyler.min.js"></script>
  		<script src="js/fonts.js"></script>
--- includes/header.php
+++ includes/header.php
@@ -15,56 +15,44 @@
 	<link rel="stylesheet" href="css/fonts.css">
 	<link rel="stylesheet" href="css/style.css">
 
+	<link rel="stylesheet" href="block/breadcrumps/style.css">
+	<link rel="stylesheet" href="block/button/style.css">
+	<link rel="stylesheet" href="block/call/style.css">
+	<link rel="stylesheet" href="block/checkboxandradio/style.css">
+	<link rel="stylesheet" href="block/counter/style.css">
+	<link rel="stylesheet" href="block/filter/style.css">
 	<link rel="stylesheet" href="block/h1/style.css">
 	<link rel="stylesheet" href="block/h2/style.css">
 	<link rel="stylesheet" href="block/h3/style.css">
 	<link rel="stylesheet" href="block/h4/style.css">
 	<link rel="stylesheet" href="block/inputtext/style.css">
-	<link rel="stylesheet" href="block/textarea/style.css">
-	<link rel="stylesheet" href="block/register-and-cart/style.css">
 	<link rel="stylesheet" href="block/logobox/style.css">
-	<link rel="stylesheet" href="block/category/style.css">
-	<link rel="stylesheet" href="block/counter/style.css">
-	<link rel="stylesheet" href="block/call/style.css">
-	<link rel="stylesheet" href="block/link/link.css">
-	<link rel="stylesheet" href="block/address/address.css">
-	<link rel="stylesheet" href="block/search/search.css">
-	<link rel="stylesheet" href="block/button/style.css">
-	<link rel="stylesheet" href="block/order/style.css">
-	<link rel="stylesheet" href="block/socials/socials.css">
-	<link rel="stylesheet" href="block/q/style.css">
-	<link rel="stylesheet" href="block/mape/style.css">
-	<link rel="stylesheet" href="block/manager/style.css">
-	<link rel="stylesheet" href="block/sidebar-menu/main-menu.css">
-	<link rel="stylesheet" href="block/header-contact/header-contact.css">
-	<link rel="stylesheet" href="block/buket/style.css">
 	<link rel="stylesheet" href="block/main-menu/main-menu.css">
-	<link rel="stylesheet" href="block/checkboxandradio/style.css">
-	<link rel="stylesheet" href="block/breadcrumps/style.css">
-	<link rel="stylesheet" href="block/ul/style.css">
-	<link rel="stylesheet" href="block/ol/style.css">
-	<link rel="stylesheet" href="block/select/jquery.formstyler.css">
+	<link rel="stylesheet" href="block/manager/style.css">
+	<link rel="stylesheet" href="block/mape/style.css">
+	<link rel="stylesheet" href="block/modal/style.css">
+	<link rel="stylesheet" href="block/pagination/style.css">
+	<link rel="stylesheet" href="block/q/style.css">
+	<link rel="stylesheet" href="block/question/style.css">
+	<link rel="stylesheet" href="block/search/search.css">
 	<link rel="stylesheet" href="block/select/style.css">
-	<link rel="stylesheet" href="block/table/style.css">
+	<link rel="stylesheet" href="block/select/jquery.formstyler.css">
 	<link rel="stylesheet" href="block/show-price/style.css">
+	<link rel="stylesheet" href="block/sidebar-menu/main-menu.css">
+	<link rel="stylesheet" href="block/socials/socials.css">
+	<link rel="stylesheet" href="block/table/style.css">
+	<link rel="stylesheet" href="block/textarea/style.css">
 	<link rel="stylesheet" href="block/tovar-img/style.css">
 	<link rel="stylesheet" href="block/tovar-item/style.css">
-	<link rel="stylesheet" href="block/filter/style.css">
-	<link rel="stylesheet" href="block/question/style.css">
-	<link rel="stylesheet" href="block/pagination/style.css">
-	<link rel="stylesheet" href="block/modal/style.css">
+	<link rel="stylesheet" href="block/ul/style.css">
 
 
 
 	<link rel="stylesheet" href="css/style-db.css">
-	<link rel="stylesheet" href="css/style-dc.css">
 	<link rel="stylesheet" href="css/style-dd.css">
 	<link rel="stylesheet" href="css/style-de.css">
 	<link rel="stylesheet" href="css/style-df.css">
 	<link rel="stylesheet" href="css/style-dg.css">
-	<link rel="stylesheet" href="css/style-dh.css">
-	<link rel="stylesheet" href="css/style-di.css">
-	<link rel="stylesheet" href="css/style-dj.css">
 
 
 </head>
