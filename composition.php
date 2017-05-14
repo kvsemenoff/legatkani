bafee11d24ca39a9bad884c3c549b605882721a4
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-14 12:45:39 +0600

    composition end

--- composition.php
+++ composition.php
@@ -6,7 +6,183 @@
 	</div>
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">
-
+			<div class="inner-de composition-page">
+				<div class="container">
+					<div class="row">
+						<div class="h1-wrap">
+							<h1 class="h1">Состав заказа</h1>
+						</div>
+						<div class="breadcrumps-wrap">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Главная</a></li>				
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg">Личнй кабинет</a></li>				
+								<li class="breadcrumps__link"><span>Состав заказа</span></li>
+							</ul>
+						</div>
+						<div class="composition-in clearfix">
+							<div class="manager clearfix">
+								<div class="manager__img">
+									<img src="img/manager.jpg" alt="">
+								</div>
+								<div class="manager__info">
+									<span>Ваш менеджер:</span>
+									<strong>Михайлова Кристина</strong>
+									<i>тел. +7 7 916 455-45-00</i>
+									<a class="manager__mail" href="#">krestama@gmai.com</a>
+									<a class="manager__question" href="#">Задать вопрос менеджеру</a>
+								</div>
+							</div>
+							<div class="composition-order">
+								<div class="order__back">
+									<a href="#"><i></i>Назад</a>
+								</div>
+								<div class="order">
+									<div class="order__line">
+										<span>Заказ #:</span>
+										<i>1</i>
+									</div>
+									<div class="order__line">
+										<span>Дата:</span>
+										<i>12.05.2017</i>
+									</div>
+									<div class="order__line">
+										<span>Сумма заказа:</span>
+										<i>2 500 руб.</i>
+									</div>
+									<div class="order__line">
+										<span>Доставка:</span>
+										<i>0 руб.</i>
+									</div>
+									<div class="order__line">
+										<span>К оплате:</span>
+										<i>2 500 руб.</i>
+									</div>
+									<div class="order__line">
+										<span>Статус:</span>
+										<i>Принят</i>
+									</div>
+									<div class="order__line">
+										<span>Оплата:</span>
+										<i>не оплачен</i>
+									</div>
+									<div class="order__re">
+										<a href="#">Повторить заказ</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="showprice__wrap">
+					    <a href="#" class="showprice_link">Показать цены в У.Е.</a>
+					  </div>
+						<div class="basket-list basket-list_composition">
+							<div class="item item__title clearfix">
+								<div class="item__img"></div>
+								<div class="item__descr vertical-middle">
+									<span>Наименование</span>
+								</div>
+								<div class="vertical-middle item__price-one">
+									<span>Цена за 1м</span>
+								</div>
+								<div class="vertical-middle item__price-all">
+									<span>Общая стоимость</span>
+								</div>
+								<div class="vertical-middle item__sale">
+									<span>Скидка(%)</span>
+								</div>
+								<div class="vertical-middle item__price-order">
+									<span>Сумма заказа</span>
+								</div>
+							</div>
+							<div class="item clearfix">
+								<div class="item__img">
+									<img src="img/tkan.jpg" alt="">
+								</div>
+								<div class="vertical-middle item__descr">
+									<span>Ткань Жаккард</span>
+									<i>Арт. 25-54558</i>
+								</div>
+								<div class="vertical-middle item__price-one">
+									<span>1000 руб.</span>
+								</div>
+								<div class="vertical-middle item__price-all">
+									<span>3000 руб.</span>
+								</div>
+								<div class="vertical-middle item__sale">
+									<span>15</span>
+								</div>
+								<div class="vertical-middle item__price-order">
+									<span>3500 руб.</span>
+								</div>
+							</div>
+							<div class="item clearfix">
+								<div class="item__img">
+									<img src="img/tkan1.jpg" alt="">
+								</div>
+								<div class="vertical-middle item__descr">
+									<span>Пальтовая ткань однотонная</span>
+									<i>Арт. 25-54558</i>
+								</div>
+								<div class="vertical-middle item__price-one">
+									<span>1000 руб.</span>
+								</div>
+								<div class="vertical-middle item__price-all">
+									<span>3000 руб.</span>
+								</div>
+								<div class="vertical-middle item__sale">
+									<span>15</span>
+								</div>
+								<div class="vertical-middle item__price-order">
+									<span>3500 руб.</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="basket-list basket-list_composition-mini">
+					<div class="item clearfix">
+						<div class="item-in">
+							<div class="item__mob item__num">
+								<span>1</span>
+							</div>
+							<div class="item__mob item__stats">
+								<span>принят</span>
+							</div>
+							<div class="item__mob item__total">
+								<span>3 500 руб.</span>
+								<a href="#">Подробнее ></a>
+							</div>
+						</div>
+					</div>
+					<div class="item clearfix">
+						<div class="item-in">
+							<div class="item__mob item__num">
+								<span>1</span>
+							</div>
+							<div class="item__mob item__stats">
+								<span>принят</span>
+							</div>
+							<div class="item__mob item__total">
+								<span>3 500 руб.</span>
+								<a href="#">Подробнее ></a>
+							</div>
+						</div>
+					</div>
+					<div class="item clearfix">
+						<div class="item-in">
+							<div class="item__mob item__num">
+								<span>1</span>
+							</div>
+							<div class="item__mob item__stats">
+								<span>принят</span>
+							</div>
+							<div class="item__mob item__total">
+								<span>3 500 руб.</span>
+								<a href="#">Подробнее ></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="n-footer">
 			<?php require_once('includes/footer.php'); ?>
@@ -15,11 +191,3 @@
 </div>
 </body>
 </html>
-
-
-
-
-
-
-
-
