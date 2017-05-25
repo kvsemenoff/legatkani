332f9b545a37d546b3742aa7db04018f4f33cab1
engine77 <zaur.1kager@gmail.com>
2017-05-25 15:49:24 +0400

    all file

--- includes/header-top.php
+++ includes/header-top.php
@@ -13,19 +13,19 @@
       <div class="col-xs-6 col-sm-6 col-md-6">
         <div class="header_auth">
           <ul class="header_auth_list">
-            <li class="hidden-xs">
+            <li class="hidden-xs header_auth_login">
               <a href="#" class="header_auth_list_link signin">Войти</a>
             </li>
-            <li class="hidden-xs">
+            <li class="hidden-xs header_auth_registration">
               <a href="#" class="header_auth_list_link signin">Регистрация</a>
             </li>
             <li class="hidden-sm hidden-md hidden-lg authorize">
               <a href="#" class="header_auth_list_link auth"></a>
             </li>
-            <li class="fav__li">
+            <li class="fav__li header_auth_favorit">
               <a href="#" class="header_auth_list_link favourite">0</a>
             </li>
-            <li>
+            <li class="header_auth_busket">
               <a href="#" class="header_auth_list_link basket">2</a>
             </li>
           </ul>
