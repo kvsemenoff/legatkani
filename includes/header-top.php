1a5f4d482389b5b31c5e404ccce1ea9e13976ad1
kvsemenoff <kvsemenoff@mail.ru>
2017-05-25 19:10:15 +0400

    d

--- includes/header-top.php
+++ includes/header-top.php
@@ -1,35 +1,23 @@
 <div class="header-top">
-  <div class="container">
-      <div class="col-xs-6 col-sm-6 col-md-6">
-        <div class="search-wrap hidden-sm hidden-md hidden-lg"> <!-- Мобайл -->
+    <div class="container">
+        <div class="col-xs-6 col-sm-6 col-md-6">
+            <div class="search-wrap hidden-sm hidden-md hidden-lg"> <!-- Мобайл -->
     			<input type="text" placeholder="Поиск...">
     			<input type="submit">
     		</div>
-        <div class="search-wrap hidden-xs"> <!-- Десктоп -->
+            <div class="search-wrap hidden-xs"> <!-- Десктоп -->
     			<input type="text" placeholder="Поиск по сайту...">
     			<input type="submit">
     		</div>
-      </div>
-      <div class="col-xs-6 col-sm-6 col-md-6">
-        <div class="header_auth">
-          <ul class="header_auth_list">
-            <li class="hidden-xs header_auth_login">
-              <a href="#" class="header_auth_list_link signin">Войти</a>
-            </li>
-            <li class="hidden-xs header_auth_registration">
-              <a href="#" class="header_auth_list_link signin">Регистрация</a>
-            </li>
-            <li class="hidden-sm hidden-md hidden-lg authorize">
-              <a href="#" class="header_auth_list_link auth"></a>
-            </li>
-            <li class="fav__li header_auth_favorit">
-              <a href="#" class="header_auth_list_link favourite">0</a>
-            </li>
-            <li class="header_auth_busket">
-              <a href="#" class="header_auth_list_link basket">2</a>
-            </li>
-          </ul>
         </div>
-      </div>
-  </div>
+        <div class="col-xs-6 col-sm-6 col-md-6">
+            <div class="header_auth">
+                <a href="#" class="header_auth_item hidden-xs signin">Войти</a>
+                <a href="#" class="header_auth_item hidden-xs signin">Регистрация</a>
+                <a href="#" class="header_auth_item hidden-sm hidden-md hidden-lg authorize auth"></a>
+                <span class="header_auth_item favourite"><a href="#" class="header_auth_link">0</a></span>
+                <span class="header_auth_item basket"><a href="#" class="header_auth_link">0</a></span>
+            </div>
+        </div>
+    </div>
 </div>
